1ada7e649181063f14b74666a62dd3f0deabf343
Andrei Mahalean <andrei@maha.nz>
2024-08-24 14:08:09 +1200

    gcloud

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,10 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [ 
+    pkgs.git
+    pkgs.google-cloud-sdk
+  ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
